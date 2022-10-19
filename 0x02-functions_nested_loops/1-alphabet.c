2826115b7a94a5e4f67256d9a38d9cd3af172cab
kofijeh <kofijeh@gmail.com>
2022-10-18 19:13:15 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,11 @@
  */
 void print_alphabet(void)
 {
-	char la;
+	char c;
 
-	for (la - 'a'; la <= 'z'; la++)
-		_putchar(la);
-
+	for (c - 'a'; c <= 'z'; c++)
+{
+		_putchar(c);
+}
 	_putchar('\n');
 }
