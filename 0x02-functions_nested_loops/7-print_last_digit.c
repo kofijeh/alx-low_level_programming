ce45a6985bcf65484b4131d4eaed2b857f2e9445
kofijeh <kofijeh@gmail.com>
2022-10-19 15:33:38 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,12 @@
  */
 int print_last_digit(int n)
 {
-	int ld  = n % 10;
+	int x  = n % 10;
 
-	if (ld < 0)
-		ld *= -1;
+	if (n < 0)
+		n = -1;
 
-	_putchar(ld + '0');
+	_putchar(n + '0');
 
-	return (0);
+	return (x);
 }
