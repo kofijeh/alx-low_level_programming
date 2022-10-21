02935ecf7c1a5f49d4d1d13edede284485548c7f
kofijeh <kofijeh@gmail.com>
2022-10-21 00:34:46 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,11 @@ int main(void)
 
 	i = 0;
 	/**
-	 * while (1 < 10)
+	 * while (i < 10)
 	 * {
-	 *	putchar(1);
+	 *	putchar(i);
 	 * }
 	 */
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
