ac193b29b87913831ef06d6e1563e4f2638d4837
kofijeh <kofijeh@gmail.com>
2022-10-21 00:52:31 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,11 @@
-#ifndef MAIN.h
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
+
 void positive_ornegative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-#endif
+#endif /* MAIN_H */
