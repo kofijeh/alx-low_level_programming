a9f9f22251108bf15c2f3c9dd67585e8a814b8b2
kofijeh <kofijeh@gmail.com>
2022-10-21 05:45:58 -0700

    Print with putchar

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: first input numer
+ * @b: second input number
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
