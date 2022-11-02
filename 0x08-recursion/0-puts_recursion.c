625987e2db61d4a53eaca1c8b165f5ab8ec80e6b
kofijeh <kofijeh@gmail.com>
2022-11-02 08:14:14 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string
+ * return: 0
+ */
+void _puts_recursion(char *s)
+{
+	char s = "string of characters"
+		_putchar(%c/n, s);
+	return (0);
+}
