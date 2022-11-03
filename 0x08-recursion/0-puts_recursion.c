2321b54d66adc68b6de867c7b45acd1b79a12ec1
kofijeh <kofijeh@gmail.com>
2022-11-03 05:11:28 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
-	if (!*s)
-	{
-		_print_rev_recursion(s + 1);
-		_putchar("\n");
-	}
+	_putchar(*s);
+	s++
+		_puts_recursion(s);
 }
