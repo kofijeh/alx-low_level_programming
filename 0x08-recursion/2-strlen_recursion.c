52d28c85d05688f4d79137934e4049ec7feb54c5
kofijeh <kofijeh@gmail.com>
2022-11-03 06:38:45 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - lenght of string
+ * @s: string
+ * Return: lenght of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	s++;
+	return (1 + _strlen_recursion(s));
+}
