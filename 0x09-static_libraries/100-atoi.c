a51fdb0dd9786837c3422dea0c1efe483c5b0fd8
kofijeh <kofijeh@gmail.com>
2022-11-15 16:57:06 -0800

    I finally did static lib

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _atoi - I don't know what this is for. Creating an empty file with it
+ * @s: character in the prototype
+ * Return: unspecified
+ */
+int _atoi(char *s)
+{
+	*s = 'H';
+
+	return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+Just learnt static libraries and coming to pratice
